aacb44092025236f0444b11ad0ec35bbdd5f3e4a
Ida Riyanti <marieyunus16@gmail.com>
2026-03-30 23:29:45 +0800

    Refactor user mention handling in status_utils.py
    Signed-off-by: Ida Riyanti <marieyunus16@gmail.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -233,7 +233,14 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
             msg += f"\n┖ <b>Sub Name</b> → <i>{task.listener.subname}</i>"
         elapsed = time() - task.listener.message.date.timestamp()
 
-        msg += f"\n\n<b>Task By {task.listener.message.from_user.mention(style='html')} </b> ( #ID{task.listener.message.from_user.id} )"
+        user = task.listener.message.from_user
+    if user:
+        msg += f"\n\n<b>Task By {user.mention(style='html')} </b> ( #ID{user.id} )"
+    else:
+        sender = task.listener.message.sender_chat
+        name = sender.title if sender else "Sistem Auto/RSS"
+        uid = sender.id if sender else "Unknown"
+        msg += f"\n\n<b>Task By {name} </b> ( #ID{uid} )"
         if task.listener.is_super_chat:
             msg += f" <i>[<a href='{task.listener.message.link}'>Link</a>]</i>"
 
