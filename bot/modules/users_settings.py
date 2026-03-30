de0e527953015d19d7fcdeabf2c615c23592364f
Ida Riyanti <marieyunus16@gmail.com>
2026-03-30 23:26:39 +0800

    Fix user ID handling in send_user_settings function
    Signed-off-by: Ida Riyanti <marieyunus16@gmail.com>

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -932,7 +932,8 @@ async def update_user_settings(query, stype="main"):
 @new_task
 async def send_user_settings(_, message):
     from_user = message.from_user
-    handler_dict[from_user.id] = False
+    user_id = message.from_user.id if message.from_user else message.sender_chat.id
+    handler_dict[user_id] = False
     msg, button = await get_user_settings(from_user)
     await send_message(message, msg, button)
 
