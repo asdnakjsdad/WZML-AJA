645146b06b78d6b9cf40098e25eb3f670734eaa0
Ida Riyanti <marieyunus16@gmail.com>
2026-03-31 11:02:47 +0800

    Simplify task ID message in status_utils.py
    Removed redundant task ID message formatting.
    
    Signed-off-by: Ida Riyanti <marieyunus16@gmail.com>

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -248,7 +248,6 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
             uid = "Unknown"
     
         msg += f"\n\n<b>Task By {name} </b> ( #ID{uid} )"
-        msg += f"\n\n<b>Task By {name} </b> ( #ID{user.id if user else 'N/A'} )"
         if task.listener.is_super_chat:
             msg += f" <i>[<a href='{task.listener.message.link}'>Link</a>]</i>"
 
